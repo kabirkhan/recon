5465cbc4f41d3c07eddd385a6dee8e90a082fe67
Kabir Khan <kakh@microsoft.com>
2020-02-27 15:23:13 -0800

    set version to 0.1.0 since breaking module name change

--- recon/__init__.py
+++ recon/__init__.py
@@ -1,6 +1,6 @@
 """ReconNER, Debug annotated Named Entity Recognition (NER) data for inconsitencies and get insights on improving the quality of your data."""
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 from .dataset import *
 from .insights import *
