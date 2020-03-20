9c10c510a8085de57354ac3b6596fbb3d00935ba
Kabir Khan <kakh@microsoft.com>
2020-03-20 15:42:55 -0700

    actually fix imports

--- recon/prodigy/recipes.py
+++ recon/prodigy/recipes.py
@@ -22,11 +22,10 @@ from prodigy.util import (
     set_hashes,
     split_string,
 )
-from wasabi import msg
-
 from recon.constants import NONE
 from recon.types import HardestExample
 from recon.validation import remove_overlapping_entities
+from wasabi import msg
 
 
 def make_span_hash(span: Dict):
