3ea59985b5c6bd2ad3b8e071a61e90732e42fec8
Kabir Khan <kakh@microsoft.com>
2020-04-11 15:09:46 -0700

    Typing and remove .pytest_cache

--- recon/store.py
+++ recon/store.py
@@ -1,49 +1,60 @@
 from pathlib import Path
-from typing import Any, Dict, List, Set
+from typing import Any, Dict, List, Set, Union
 
 from spacy.util import ensure_path
 import srsly
 
-from .hashing import text_hash
 from .types import Example
 
 
 class ExampleStore:
     def __init__(self, examples: List[Example] = None):
         self._map: Dict[int, Example] = {}
-        self._texts: Set[str] = set()
         if examples is not None:
             for e in examples:
                 self.add(e)
 
-    def __getitem__(self, example_hash: int):
+    def __getitem__(self, example_hash: int) -> Example:
         return self._map[example_hash]
     
-    def add(self, example: Example):
-        example_hash = hash(example)
-        self._map[example_hash] = example
-        self._texts.add(text_hash(example.text))
-    
-    def __len__(self):
+    def __len__(self) -> int:
         """The number of strings in the store.
-        RETURNS (int): The number of strings in the store.
+
+        Returns:
+            Number of examples in store
         """
         return len(self._map)
 
-    def __contains__(self, example):
+    def __contains__(self, example: Union[int, Example]) -> bool:
         """Check whether a string is in the store.
-        string (unicode): The string to check.
-        RETURNS (bool): Whether the store contains the string.
+        
+        Args:
+            example (Union[int, Example]): The example to check
+
+        Returns:
+            Whether the store contains the example.
         """
-        return hash(example) in self._map
+        example_hash = hash(example) if isinstance(example, Example) else example
+        return example_hash in self._map
     
-    def contains(self, example):
-        return example in self
-    
-    def contains_text(self, example):
-        return example.text in self._texts
+    def add(self, example: Example) -> None:
+        """Add an Example to the store
+        
+        Args:
+            example (Example): example to add
+        """
+        example_hash = hash(example)
+        self._map[example_hash] = example
 
     def from_disk(self, path: Path) -> "ExampleStore":
+        """Load store from disk
+
+        Args:
+            path (Path): Path to file to load from
+        
+        Returns:
+            ExampleStore: Initialized ExampleStore
+        """        
         path = ensure_path(path)
         examples = srsly.read_jsonl(path)
         for e in examples:
@@ -56,7 +67,11 @@ class ExampleStore:
         return self
     
     def to_disk(self, path: Path) -> None:
-
+        """Save store to disk
+        
+        Args:
+            path (Path): Path to save store to
+        """
         path = ensure_path(path)
         examples = []
         for example_hash, example in self._map.items():
