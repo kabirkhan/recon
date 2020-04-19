88417b9fb549652ded88fda16c6af55c73b4b90c
Kabir Khan <kakh@microsoft.com>
2020-04-19 14:40:58 -0700

    Re-adding operations preprocessing code accidentally checked out

--- recon/operations.py
+++ recon/operations.py
@@ -1,5 +1,5 @@
 import functools
-from collections import Counter
+from collections import Counter, defaultdict
 from copy import deepcopy
 from inspect import isclass
 from typing import Any, Callable, Dict, Iterator, List, Set, Tuple, Union
@@ -7,6 +7,7 @@ from typing import Any, Callable, Dict, Iterator, List, Set, Tuple, Union
 import catalogue
 import srsly
 
+from .preprocess import PreProccessor
 from .types import (
     Example,
     OperationResult,
@@ -22,7 +23,9 @@ class registry:
     operations = catalogue.create("recon", "operations", entry_points=True)
 
 
-def op_iter(data: List[Example]) -> Iterator[Tuple[int, Example]]:
+def op_iter(
+    data: List[Example], pre: List[PreProccessor]
+) -> Iterator[Tuple[int, Example, Dict[str, Any]]]:
     """Iterate over list of examples for an operation
     yielding tuples of (example hash, example)
     
@@ -32,12 +35,18 @@ def op_iter(data: List[Example]) -> Iterator[Tuple[int, Example]]:
     Yields:
         Iterator[Tuple[int, Example]]: Tuples of (example hash, example)
     """
+    preprocessed_outputs: Dict[Example, Dict[str, Any]] = defaultdict(dict)
+    for processor in pre:
+        processor_outputs = processor(data)
+        for example, output in processor_outputs:
+            preprocessed_outputs[example][processor.name] = processor_outputs
+
     for example in data:
-        yield hash(example), example.copy(deep=True)
+        yield hash(example), example.copy(deep=True), preprocessed_outputs[example]
 
 
 class operation:
-    def __init__(self, name: str, batch: bool = False):
+    def __init__(self, name: str, pre: List[PreProccessor] = []):
         """Decorate an operation that makes some changes to a dataset.
 
         Args:
@@ -45,7 +54,7 @@ class operation:
             batch (bool): Send all examples in dataset for batch operation. 
         """
         self.name = name
-        self.batch = batch
+        self.pre = pre
 
     def __call__(self, *args: Any, **kwargs: Any) -> Callable:
         """Decorator for an operation. 
@@ -65,7 +74,7 @@ class operation:
             Callable: Original function
         """
         op: Callable = args[0]
-        registry.operations.register(self.name)(Operation(self.name, self.batch, op))
+        registry.operations.register(self.name)(Operation(self.name, self.pre, op))
 
         return op
 
@@ -74,7 +83,7 @@ class Operation:
     """Operation class that takes care of calling and reporting
     the results of an operation on a Dataset"""
 
-    def __init__(self, name: str, batch: bool, op: Callable):
+    def __init__(self, name: str, pre: List[PreProccessor], op: Callable):
         """Initialize an Operation instance
         
         Args:
@@ -83,7 +92,7 @@ class Operation:
             op (Callable): Decorated function
         """
         self.name = name
-        self.batch = batch
+        self.pre = pre
         self.op = op
 
     def __call__(self, dataset: Any, *args: Any, **kwargs: Any) -> OperationResult:
@@ -129,47 +138,31 @@ class Operation:
             )
             dataset.example_store.add(new_example)
 
-        def track_example(orig_example_hash: int = None, new_example: Example = None) -> None:
-            if orig_example_hash and not new_example:
-                remove_example(orig_example_hash)
-            elif new_example and not orig_example_hash:
-                add_example(new_example)
-            elif orig_example_hash and new_example:
-                if orig_example_hash != hash(new_example):
-                    change_example(orig_example_hash, new_example)
+        new_data = []
+        for orig_example_hash, example, preprocessed_outputs in op_iter(dataset.data, self.pre):
+            if preprocessed_outputs:
+                res = self.op(example, *args, preprocessed_outputs=preprocessed_outputs, **kwargs)
             else:
-                raise ValueError("Error tracking example, no data sent")
+                res = self.op(example, *args, **kwargs)
 
-        callbacks = TransformationCallbacks(
-            add_example=add_example,
-            remove_example=remove_example,
-            change_example=change_example,
-            track_example=track_example,
-        )
-
-        if self.batch:
-            new_data = self.op(dataset.data, *args, **kwargs, callbacks=callbacks)
-        else:
-            new_data = []
-            for orig_example_hash, example in op_iter(dataset.data):
-                res: Union[Example, List[Example], None] = self.op(example, *args, **kwargs)
-                if res is None:
+            if res is None:
+                remove_example(orig_example_hash)
+            elif isinstance(res, list):
+                old_example_present = False
+                for new_example in res:
+                    if hash(new_example) == orig_example_hash:
+                        old_example_present = True
+                    else:
+                        new_data.append(new_example)
+                        add_example(new_example)
+                if not old_example_present:
                     remove_example(orig_example_hash)
-                elif isinstance(res, list):
-                    old_example_present = False
-                    for new_example in res:
-                        if hash(new_example) == orig_example_hash:
-                            old_example_present = True
-                        else:
-                            new_data.append(new_example)
-                            add_example(new_example)
-                    if not old_example_present:
-                        remove_example(orig_example_hash)
-                else:
-                    assert isinstance(res.text, str)
-                    assert isinstance(res.spans, list)
-                    new_data.append(res)
-                    track_example(orig_example_hash, res)
+            else:
+                assert isinstance(res.text, str)
+                assert isinstance(res.spans, list)
+                new_data.append(res)
+                if hash(res) != orig_example_hash:
+                    change_example(orig_example_hash, res)
 
         transformation_counts = Counter([t.type for t in state.transformations])
 
