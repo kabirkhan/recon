6f33ae6d24afb34900c14782b1cfb9a730f07a21
Kabir Khan <kakh@microsoft.com>
2020-05-19 19:35:48 -0700

    Bump version

--- recon/__init__.py
+++ recon/__init__.py
@@ -1,6 +1,6 @@
 """ReconNER, Debug annotated Named Entity Recognition (NER) data for inconsitencies and get insights on improving the quality of your data."""
 
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 
 from .corpus import Corpus
 from .corrections import *
