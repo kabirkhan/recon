abf4ded4bf4ecf476bc569673ba8620c70b7b3f3
Kabir Khan <kakh@microsoft.com>
2020-05-24 09:29:04 -0700

    Fix tests and tokenization flag for spacy vs prodigy format

--- docs/src/tutorial/3_dataset_mutate_save.py
+++ docs/src/tutorial/3_dataset_mutate_save.py
@@ -7,7 +7,7 @@ from recon.stats import get_ner_stats
 
 def main(data_file: Path, output_file: Path):
     ds = Dataset("train").from_disk(data_file)
-    
+
     print("STATS BEFORE")
     print("============")
     print(ds.apply(get_ner_stats, serialize=True))
@@ -19,7 +19,7 @@ def main(data_file: Path, output_file: Path):
     print(ds.apply(get_ner_stats, serialize=True))
 
     ds.to_disk(output_file, force=True)
-    
+
 
 if __name__ == "__main__":
     typer.run(main)
