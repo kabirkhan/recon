e51984b0d2eb3ccea93f42b6f47cc566a526e382
Kabir Khan <kabir@subliminal.ai>
2020-08-02 12:02:31 -0700

    Split prodigy tests into conditional module. Bump version to 0.6.0

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -69,20 +69,3 @@ def test_corpus_apply_inplace(example_corpus_processed):
     assert len(example_corpus_processed.train_ds.operations) == 4
     assert len(example_corpus_processed.dev_ds.operations) == 4
     assert len(example_corpus_processed.test_ds.operations) == 4
-
-
-def test_corpus_to_from_prodigy(example_corpus):
-    prodigy_train_dataset, prodigy_dev_dataset, prodigy_test_dataset = example_corpus.to_prodigy(
-        overwrite=True
-    )
-    name = example_corpus.name
-
-    corpus_loaded = Corpus.from_prodigy(
-        name, [prodigy_train_dataset], [prodigy_dev_dataset], [prodigy_test_dataset]
-    )
-
-    assert example_corpus.name == corpus_loaded.name
-    assert len(example_corpus.train) == len(corpus_loaded.train)
-    # assert len(example_corpus.dev) == len(corpus_loaded.dev)
-    # assert len(example_corpus.test) == len(corpus_loaded.test)
-    assert len(example_corpus.all) == len(corpus_loaded.all)
---
+++ tests/test_prodigy.py
@@ -0,0 +1,22 @@
+import pytest
+from recon.corpus import Corpus
+
+
+prodigy = pytest.importorskip("prodigy")
+
+
+def test_corpus_to_from_prodigy(example_corpus):
+    prodigy_train_dataset, prodigy_dev_dataset, prodigy_test_dataset = example_corpus.to_prodigy(
+        overwrite=True
+    )
+    name = example_corpus.name
+
+    corpus_loaded = Corpus.from_prodigy(
+        name, [prodigy_train_dataset], [prodigy_dev_dataset], [prodigy_test_dataset]
+    )
+
+    assert example_corpus.name == corpus_loaded.name
+    assert len(example_corpus.train) == len(corpus_loaded.train)
+    # assert len(example_corpus.dev) == len(corpus_loaded.dev)
+    # assert len(example_corpus.test) == len(corpus_loaded.test)
+    assert len(example_corpus.all) == len(corpus_loaded.all)
