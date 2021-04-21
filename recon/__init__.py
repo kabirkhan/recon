c76e0dadef5b3bd15a3a965bd1f818ad7a23205a
Kabir Khan <kabir@subliminal.ai>
2021-04-20 20:47:59 -0700

    bump version 0.8.2

--- recon/__init__.py
+++ recon/__init__.py
@@ -1,6 +1,6 @@
 """ReconNER, Debug annotated Named Entity Recognition (NER) data for inconsitencies and get insights on improving the quality of your data."""
 
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 
 from recon.augmentation import *
 from recon.corpus import Corpus
