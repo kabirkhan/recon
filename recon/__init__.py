c9cfef914e75223ba6a0650d7bb7d2f9b0207ee5
Kabir Khan <kabir@explosion.ai>
2023-09-02 13:54:39 -0700

    bump to 0.14.1

--- recon/__init__.py
+++ recon/__init__.py
@@ -1,7 +1,7 @@
 """ReconNER, Debug annotated Named Entity Recognition (NER) data for
 inconsitencies and get insights on improving the quality of your data."""
 
-__version__ = "0.14.0"
+__version__ = "0.14.1"
 
 from recon.corpus import Corpus
 from recon.dataset import Dataset
